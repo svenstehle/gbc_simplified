b51878ab23ff046852ea822c001c9bbf29720bd8
stehles <svenstehle@web.de>
2022-07-21 23:00:01 +0200

    update gbdt code for medium article and simplify

--- gradient_boosting_multiclass.py
+++ gradient_boosting_multiclass.py
@@ -5,8 +5,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import pdb
-
 import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_validate, train_test_split
@@ -41,8 +39,6 @@ print(f'The accuracy is: {predictions*100:.2f}%')
 
 # %%
 
-# heavily borrowed from scikit learn
-
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeRegressor
 
@@ -50,72 +46,71 @@ from sklearn.tree import DecisionTreeRegressor
 class GradientBoostingCustom:
     def __init__(self, n_estimators=30, learning_rate=0.1, max_depth=3, random_state=42):
         """Constructor of GradientBoostingCustom. During init, self.base_estimator will be
-        set as an instance of DummyClassifier. During the call of ``fit()``, self.base_estimator
+        set as an instance of DummyClassifier. During the call to ``fit()``, self.base_estimator
         will become the root of the gradient boosting ensemble. It is the base for the first
-        ``raw_predictions``, which are log-probabilities for each of the K classes. Based on these
-        ``raw_predictions``, we build the ensemble with each new boosting stage and further refine
-        the ``raw_predictions``.
+        ``outputs``, which are log-probabilities for each of the K classes. Based on these
+        ``outputs``, we build the ensemble with each new boosting stage and further refine
+        the ``outputs``.
 
         Args:
             n_estimators (int, optional): the number of boosting stages. Defaults to 30.
             learning_rate (float, optional): the influence of each new estimator ``i`` on the current
-                ``raw_predictions`` of stage ``i-1``. Defaults to 0.1.
+                ``outputs`` of stage ``i-1``. Defaults to 0.1.
             max_depth (int, optional): the maximum depth of each DecisionTreeRegressor.
                 Should be tuned. Note: we do not have to work with stumps here. Defaults to 3.
-            random_state (int, optional): _description_. Defaults to 42.
+            random_state (int, optional): the seed for the random number
+              generator. Defaults to 42.
         """
         self.n_estimators = n_estimators
-        self.lr = learning_rate
+        self.learning_rate = learning_rate
         self.max_depth = max_depth
         self.random_state = random_state
-        self.estimators = None
+        self.ensemble = None
         self.classes_ = None
         self.n_classes_ = None
         self.base_estimator = DummyClassifier(strategy="prior")
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+        """Fits the algorithm on the input features ``X``
+        and the Target labels ``y``.
+
+        Args:
+            X (np.ndarray of shape (n_samples, K)): input features.
+            y (np.ndarray of shape (n_samples, )): Target labels.
+
+        Returns:
+            GradientBoostingCustom: instance of self.
+        """
         random_state = np.random.RandomState(self.random_state)
 
-        # get fit base estimator and get initial outputs
+        # get fit base estimator and initial outputs
         self.base_estimator.fit(X, y)
-        outputs = self._get_init_outputs(X, self.base_estimator)
+        outputs = self._get_initial_outputs(X, self.base_estimator)
 
         # get the class information
         self.classes_ = np.unique(y)
         self.n_classes_ = len(self.classes_)
 
         # create an array that holds ``n_estimators`` for each ``n_classes`` ("ensemble of trees")
-        self.estimators = np.empty((self.n_estimators, self.n_classes_), dtype=object)
+        self.ensemble = np.empty((self.n_estimators, self.n_classes_), dtype=object)
 
-        # compute the residuals for each class and each boost stage
+        # compute the residuals for each boost stage and class
         for iboost in range(self.n_estimators):
-            # outputs are in log-probability
-            outputs = self._fit_stage(iboost, X, y, outputs, random_state)
+            for k in range(self.n_classes_):
+                # get the negative gradient (residuals) for each class
+                residuals = self._compute_residuals(y, outputs, k)
+
+                # fit regression tree on residuals
+                tree = DecisionTreeRegressor(max_depth=self.max_depth, splitter="best", random_state=random_state)
+                tree.fit(X, residuals)
+
+                # update predictions for class k with current regression tree
+                outputs[:, k] = self._update_outputs(X, outputs, k, tree)
+
+                # add tree to ensemble
+                self.ensemble[iboost, k] = tree
 
         return self
-
-    def _fit_stage(
-        self,
-        iboost: int,
-        X: np.ndarray,
-        y: np.ndarray,
-        outputs: np.ndarray,
-        random_state: np.random.RandomState,
-    ):
-        for k in range(self.n_classes_):
-            # get the negative gradient (residuals) for each class
-            residuals = self._compute_residuals(y, outputs, k)
-
-            # fit regression tree on residuals
-            tree = DecisionTreeRegressor(max_depth=self.max_depth, splitter="best", random_state=random_state)
-            tree.fit(X, residuals)
-
-            # update predictions for class k with current regression tree
-            outputs[:, k] = self._update_outputs(X, outputs, k, tree)
-
-            # add tree to ensemble
-            self.estimators[iboost, k] = tree
-        return outputs
 
     def _compute_residuals(self, y: np.ndarray, outputs: np.ndarray, k: int):
         """Compute residuals for class k.
@@ -168,10 +163,25 @@ class GradientBoostingCustom:
         Returns:
             np.ndarray of shape (n_samples, ): the updated outputs for class k.
         """
-        return outputs[:, k].ravel() + self.lr * tree.predict(X)
+        return outputs[:, k].ravel() + self.learning_rate * tree.predict(X)
 
-    def _get_init_outputs(self, X: np.ndarray, estimator: DummyClassifier):
-        # initialize raw predictions as log-probabilities
+    def _get_initial_outputs(self, X: np.ndarray, estimator: DummyClassifier):
+        """Returns the initial outputs: DummyClassifier ``predict_proba`` always
+        returns the empirical class distribution of y also known as the
+        empirical class prior distribution. These probabilities are then clipped
+        and returned as log-probabilities.
+
+        Args:
+            X (np.ndarray of shape (n_samples, n_features)): input features.
+            estimator (DummyClassifier): the DummyClassifier object that will
+                be fit during this function call.
+
+        Returns:
+            np.ndarray of shape (n_samples, K): The outputs as
+                log-probabilities of the tree ensemble at
+                iteration 0 for all ``K`` classes.
+        """
+        # initialize outputs as log-probabilities
         probas = estimator.predict_proba(X)
         eps = np.finfo(np.float32).eps
         probas = np.clip(probas, eps, 1 - eps)
@@ -179,8 +189,16 @@ class GradientBoostingCustom:
         return outputs
 
     def predict(self, X: np.ndarray):
+        """Returns the predictions for input features X.
+
+        Args:
+            X (np.ndarray of shape (n_samples, n_features)): input features.
+
+        Returns:
+            np.ndarray of shape (n_samples, ): the predictions for X.
+        """
         classes = self.classes_[:, np.newaxis]
-        outputs = self._get_init_outputs(X, self.base_estimator)
+        outputs = self._get_initial_outputs(X, self.base_estimator)
 
         # make predictions for each of the k classes with the respective estimators
         for i in range(self.n_estimators):
@@ -190,16 +208,13 @@ class GradientBoostingCustom:
                     X,
                     outputs,
                     k,
-                    self.estimators[i, k][0],
+                    self.ensemble[i, k][0],
                 ).reshape(-1, 1)
 
-        proba = self._outputs_to_proba(outputs)
+        # convert log-probabilities to probability
+        proba = np.exp(outputs)
         # get class label for max probability per row
-        return self.classes_.take(np.argmax(proba, axis=1), axis=0)
-
-    def _outputs_to_proba(self, outputs: np.ndarray):
-        # convert outputs (in logprob) to probabillity
-        return np.nan_to_num(np.exp(outputs))
+        return np.argmax(proba, axis=1)
 
 
 #%%
@@ -210,10 +225,12 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 plt.style.use('fivethirtyeight')
+plt.rcParams['axes.facecolor'] = 'none'
 
 number_of_base_learners = 50
 
 fig = plt.figure(figsize=(8, 6))
+fig.patch.set_facecolor('white')
 ax0 = fig.add_subplot(111)
 accuracies_custom = []
 accuracies_sklearn = []
@@ -263,128 +280,10 @@ plt.legend(['custom', 'sklearn_gbc', 'sklearn_rf'])
 ax0.set_xlabel('# models used for Boosting ')
 ax0.set_ylabel('accuracy')
 print(
-    f'With a number of {number_of_base_learners} base models we receive an accuracy of {accuracies_custom[-1]*100:.2f}%'
+    f'With a number of {number_of_base_learners} estimators '
+    f'(boosting stages) we receive an accuracy of {accuracies_custom[-1]*100:.2f}%'
 )
 
 plt.show()
-
-# %%
-# GitHub gists work in progress
-
-# initialize and fit our first tree to the data
-tree = DecisionTreeClassifier(max_depth=1)
-tree.fit(X, y)
-outputs = tree.predict(X)
-
-#%%
-
-from sklearn.tree import DecisionTreeRegressor
-
-# total number of trees in the GBDT ensemble
-n_estimators = 10
-# the number of classes that we have
-n_classes = 3
-# array to store the trees in each boosting stage
-ensemble = np.empty((n_estimators, n_classes), dtype=object)
-
-for i in range(n_estimators):
-    for k in range(n_classes):
-        # obtain residuals
-        residuals = get_residuals(y, outputs, k)
-
-        # fit regression tree on residuals
-        tree = DecisionTreeRegressor()
-        tree.fit(X, residuals)
-
-        # update the outputs with the new predictions
-        outputs[:, k] = update_outputs(outputs, tree.predict(X), k)
-
-        # store the tree for this boost stage ``i`` in the ensemble
-        ensemble[i, k] = tree
-
-#%%
-
-ensemble = np.empty((10, 3), dtype=object)
-ensemble
-#%%
-
-# gist for residual computation
-
-# we want to compute the residuals for class k=2
->>> k = 2
-
-# our original labels look like this
->>> y = np.array([0, 1, 2, 2])
-# we will convert them to our binary encoding for k=2
->>> y = np.where(y == k, 1, 0)
-# now, y looks like this:
->>> y
-array([0, 0, 1, 1])
-
-# the current probabilities look like this
-# we have 4 samples and 3 classes, resulting in a 4x3 array
-# each row of the array sums up to 1, since we deal with probabilities
->>> probabilities_of_class_k = np.array([
-        [0.3, 0.5, 0.2],
-        [0.3, 0.2, 0.5],
-        [0.6, 0.1, 0.3],
-        [0.2, 0.1, 0.7],
-    ])
-
-# the actual entries in the ``output`` array are log-probabilities
->>> outputs = np.log(probabilities_of_class_k)
-
-# we get the log-probabilities for class k from the outputs
->>> log_probas_class_k = outputs[:, k]
->>> log_probas_class_k
-array([-1.60943791, -0.69314718, -1.2039728 , -0.35667494])
-
-# convert them to probabilities and clip them in case of NaN entries
->>> probas_class_k = np.exp(log_probas_class_k)
->>> clipped_probas_class_k = np.nan_to_num(probas_class_k)
->>> clipped_probas_class_k
-array([0.2, 0.5, 0.3, 0.7])
-
-# finally, we compute and return residuals (negative gradient)
->>> negative_gradient = y - clipped_probas_class_k
->>> negative_gradient
-array([-0.2, -0.5,  0.7,  0.3])
-
-
-
-#%%
-
-# investigate the negative gradient computation on raw predictions
-k = 2
-original_y = np.array([0, 1, 2, 2])
-
-raw_predictions = np.log(np.array([
-    [0.3, 0.5, 0.2],
-    [0.3, 0.2, 0.5],
-    [0.6, 0.1, 0.3],
-    [0.2, 0.1, 0.7],
-]))
-raw_predictions
-#%%
-
-y = np.array(original_y == k, dtype=np.float64)
-y, y.shape
-
-#%%
-# the log-probabilities for class k
-log_prob = raw_predictions[:, k]
-log_prob
-
-#%%
-# the probabilities for class k
-probas = np.exp(log_prob)
-
-# wrapper in case of nan
-probas_clipped = np.nan_to_num(probas)
-probas_clipped
-
-#%%
-# the negative gradient
-y - probas_clipped
 
 # %%
